5df83e1dc8e652c167754a41ef129ea42fba43a7
Lucas Menezes <livedolux@gmail.com>
2026-05-22 18:13:17 -0300

    setup initial navigation

---
+++ src/app/(tabs)/add-meal.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { globalStyles } from "../../styles/global";
+
+export default function AddMealScreen() {
+  return (
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.title}>Add Meal</Text>
+    </View>
+  );
+}
---
+++ src/app/(tabs)/meals.tsx
@@ -0,0 +1,10 @@
+import { ScrollView, Text } from "react-native";
+import { globalStyles } from "../../styles/global";
+
+export default function MealsScreen() {
+  return (
+    <ScrollView style={globalStyles.container}>
+      <Text style={globalStyles.title}>All Meals</Text>
+    </ScrollView>
+  );
+}
